534713fd040df4e1c705829db9a41be55dd55cb6
Sandeep Parikh <sandeep@clusterbeep.org>
2015-06-17 13:27:37 -0700

    making sample app more generic and simpler

--- sample-webapp/app.py
+++ sample-webapp/app.py
@@ -16,66 +16,28 @@
 
 
 import webapp2
-from google.appengine.ext import ndb
-
-
-class StationMetric(ndb.Model):
-    """
-    Thermometer for measuring air and sea surface temperature
-    Barometer for measuring atmospheric pressure
-    Hygrometer for measuring humidity.
-    Anemometer for measuring wind speed
-    Rain gauge for measuring liquid precipitation over a set period of time.
-    Present Weather/Precipitation Identification Sensor for identifying falling precipitation
-    Disdrometer for measuring drop size distribution
-    Transmissometer for measuring visibility
-    Ceilometer for measuring cloud ceiling
-    """
-    stationid = ndb.StringProperty()
-    recorded = ndb.DateTimeProperty(auto_now_add=True)
-    temperature = ndb.FloatProperty()
-    pressure = ndb.FloatProperty()
-    humidity = ndb.FloatProperty()
-    windspeed = ndb.FloatProperty()
-    precipitation = ndb.FloatProperty()
-    dropsize = ndb.FloatProperty()
-    visibility = ndb.FloatProperty()
-    ceiling = ndb.FloatProperty()
 
 
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Workload Simulation Using Containers as Clients\n')
+        self.response.write('Welcome to Simulating Workloads Using Containers as Clients\n')
 
 
 class LoginHandler(webapp2.RequestHandler):
     def post(self):
-        stationid = self.request.get('stationid')
+        deviceid = self.request.get('deviceid')
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('/login - station: {}\n'.format(stationid))
+        self.response.write('/login - device: {}\n'.format(deviceid))
 
 
 class MetricsHandler(webapp2.RequestHandler):
     def post(self):
-        stationid = self.request.get('stationid')
-
-        sm = StationMetric(
-            stationid = stationid,
-            temperature = float(self.request.get('temperature')),
-            pressure = float(self.request.get('pressure')),
-            humidity = float(self.request.get('humidity')),
-            windspeed = float(self.request.get('windspeed')),
-            precipitation = float(self.request.get('precipitation')),
-            dropsize = float(self.request.get('dropsize')),
-            visibility = float(self.request.get('visibility')),
-            ceiling = float(self.request.get('ceiling'))
-        )
-        smkey = sm.put()
+        deviceid = self.request.get('deviceid')
+        timestamp = self.request.get('timestamp')
         
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('/metrics - station: {}, kind: {}, id: {}\n'.
-            format(stationid, smkey.kind(), smkey.id()))
+        self.response.write('/metrics - device: {}, timestamp: {}\n'.format(deviceid, timestamp))
 
 
 app = webapp2.WSGIApplication([
